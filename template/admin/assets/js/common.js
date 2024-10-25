3a2038b93dc28f53aee009dce1f45ce50cfbeb85
kjh <kjhied@gmail.com>
2024-10-25 18:05:34 +0900

    [fix] 기본환경설정 > 주소검색 시, 참고항목이 상세주소에 붙도록 수정

--- template/admin/assets/js/common.js
+++ template/admin/assets/js/common.js
@@ -401,7 +401,8 @@ var win_zip = function(frm_name, frm_zip, frm_addr1, frm_addr2, frm_jibeon) {
 
         of[frm_zip].value = data.zonecode;
 
-        of[frm_addr1].value = fullAddr + extraAddr;
+        of[frm_addr1].value = fullAddr;
+        of[frm_addr2].value = extraAddr;
 
         if(of[frm_jibeon] !== undefined){
             of[frm_jibeon].value = data.userSelectedType;
