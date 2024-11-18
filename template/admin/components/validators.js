9f904c9ebe89a4d8e38cb0c2100a15a42cb9ebc4
dungdang39 <sth0701@icloud.com>
2024-11-18 10:02:06 +0900

    [feat] 사업자등록번호 유효성 검사 함수 추가

--- template/admin/components/validators.js
+++ template/admin/components/validators.js
@@ -4,4 +4,10 @@ export const isValidEmail = (email) => {
     return emailRegex.test(email);
 };
 
-
+// 사업자등록번호 유효성 검사 (12자리, 숫자와 하이픈만 허용)
+export const isValidBusinessNumber = (value) => {
+  const numberOnly = value.replace(/-/g, '');
+  const validCharacters = /^[\d-]+$/.test(value);
+  const validLength = numberOnly.length === 12;
+return validCharacters && validLength;
+};
